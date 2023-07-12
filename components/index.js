127a4c2163f5794dada7cb16ab14b67b17480eaa
Brandon Peterson <brandonacfuntech@gmail.com>
2023-07-12 14:42:47 -0500

    Updated components list

--- components/index.js
+++ components/index.js
@@ -1,2 +1,4 @@
 export { default as NavBar } from "./NavBar";
-export { default as SideBtns } from "./SideBtns";
+export { default as Footer } from "./Footer";
+export { default as ShareBtns } from "./ShareBtns";
+export { default as Tag } from "./Tag";
