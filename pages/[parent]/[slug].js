ed1f035252877d9b7fba020e31bb51b80ac4b6fa
Brandon Peterson <brandonacfuntech@gmail.com>
2023-07-14 11:46:43 -0500

    Okay put the head in the right place lol

--- pages/[parent]/[slug].js
+++ pages/[parent]/[slug].js
@@ -72,31 +72,6 @@ const PreComponent = (props) => {
 
   return (
     <>
-      <Head>
-        <title>{props.frontmatter.title}</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon_new.ico" />
-
-        <meta
-          name="title"
-          property="og:title"
-          content="BitBytes Developer Blog"
-        />
-        <meta property="og:type" content="Website" />
-        <meta
-          name="image"
-          property="og:image"
-          content={
-            props.frontmatter.image != undefined ? props.frontmatter.image : ""
-          }
-        />
-        {/* <meta
-          name="description"
-          property="og:description"
-          content="BitBytes Developer Blog"
-        /> */}
-        <meta name="author" content={props.frontmatter.author} />
-      </Head>
       <div className="mt-5 mb-5">
         <div className="flex items-center justify-between bg-gray-800 p-3 rounded-t-lg border-2 border-white border-b-0">
           <span className="text-white font-semibold">
@@ -171,74 +146,99 @@ export default function BlogPost({ frontmatter, mdxSource }) {
     .map((part) => decodeURIComponent(part));
 
   return (
-    <main className="w-full flex flex-col items-center mb-16">
-      <div className="mt-16 left-0 h-3 fixed w-screen flex flex-col justify-start items-start max-lg:mt-20">
-        <motion.div
-          style={{ scaleX: scrollYProgress }}
-          className="w-full h-full bg-gradient-to-r to-orange-600 from-amber-600 origin-left"
+    <>
+      <Head>
+        <title>{frontmatter.title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon_new.ico" />
+
+        <meta
+          name="title"
+          property="og:title"
+          content="BitBytes Developer Blog"
         />
-      </div>
-      <div className="w-2/4 flex flex-col mt-20 pt-5 max-lg:w-11/12 max-lg:pt-9">
-        <div className="w-full flex flex-row items-center mb-5 justify-start max-lg:text-sm">
-          <Link
-            href="/"
-            className="hover:text-orange-500 line-clamp-1 min-w-fit"
-          >
-            Home
-          </Link>
-          <BiChevronsRight className="h-6 w-6 ml-2 mr-2 max-lg:w-7" />
-          <Link
-            href={`/${pathParts[0]}`}
-            className="hover:text-orange-500 line-clamp-1 min-w-fit"
-          >
-            {pathParts[0]}
-          </Link>
-          <BiChevronsRight className="h-6 w-6 ml-2 mr-2 max-lg:w-7 max" />
-          <Link
-            href={`/${pathParts[0]}/${pathParts[1]}`}
-            className="hover:text-orange-500 line-clamp-1 min-w-fit max-lg:min-w-0"
-          >
-            {pathParts[1]}
-          </Link>
+        <meta property="og:type" content="Website" />
+        <meta
+          name="image"
+          property="og:image"
+          content={frontmatter.image != undefined ? frontmatter.image : ""}
+        />
+        {/* <meta
+          name="description"
+          property="og:description"
+          content="BitBytes Developer Blog"
+        /> */}
+        <meta name="author" content={frontmatter.author} />
+      </Head>
+      <main className="w-full flex flex-col items-center mb-16">
+        <div className="mt-16 left-0 h-3 fixed w-screen flex flex-col justify-start items-start max-lg:mt-20">
+          <motion.div
+            style={{ scaleX: scrollYProgress }}
+            className="w-full h-full bg-gradient-to-r to-orange-600 from-amber-600 origin-left"
+          />
         </div>
-        <div>
-          <h1 className="text-6xl max-lg:text-4xl">{frontmatter.title}</h1>
-          <h2 className="text-3xl mt-2 max-lg:text-xl">
-            {frontmatter.subtitle}
-          </h2>
-          <div className="flex flex-row m-2 items-center">
-            <div className="w-28 h-28 rounded-full overflow-hidden m-3 max-lg:w-16 max-lg:h-16">
-              <img
-                src={frontmatter.author_image}
-                alt={frontmatter.author}
-                className="object-cover h-full w-full scale-125"
-              />
+        <div className="w-2/4 flex flex-col mt-20 pt-5 max-lg:w-11/12 max-lg:pt-9">
+          <div className="w-full flex flex-row items-center mb-5 justify-start max-lg:text-sm">
+            <Link
+              href="/"
+              className="hover:text-orange-500 line-clamp-1 min-w-fit"
+            >
+              Home
+            </Link>
+            <BiChevronsRight className="h-6 w-6 ml-2 mr-2 max-lg:w-7" />
+            <Link
+              href={`/${pathParts[0]}`}
+              className="hover:text-orange-500 line-clamp-1 min-w-fit"
+            >
+              {pathParts[0]}
+            </Link>
+            <BiChevronsRight className="h-6 w-6 ml-2 mr-2 max-lg:w-7 max" />
+            <Link
+              href={`/${pathParts[0]}/${pathParts[1]}`}
+              className="hover:text-orange-500 line-clamp-1 min-w-fit max-lg:min-w-0"
+            >
+              {pathParts[1]}
+            </Link>
+          </div>
+          <div>
+            <h1 className="text-6xl max-lg:text-4xl">{frontmatter.title}</h1>
+            <h2 className="text-3xl mt-2 max-lg:text-xl">
+              {frontmatter.subtitle}
+            </h2>
+            <div className="flex flex-row m-2 items-center">
+              <div className="w-28 h-28 rounded-full overflow-hidden m-3 max-lg:w-16 max-lg:h-16">
+                <img
+                  src={frontmatter.author_image}
+                  alt={frontmatter.author}
+                  className="object-cover h-full w-full scale-125"
+                />
+              </div>
+              <div>
+                <h2 className="m-2 ml-0 text-2xl max-lg:text-lg">
+                  {frontmatter.author}
+                </h2>
+                <h2 className="m-2 ml-0">{`Last updated ${frontmatter.date}`}</h2>
+                {frontmatter.read_time != undefined ? (
+                  <h3>{`${frontmatter.read_time} read`}</h3>
+                ) : (
+                  <></>
+                )}
+              </div>
             </div>
-            <div>
-              <h2 className="m-2 ml-0 text-2xl max-lg:text-lg">
-                {frontmatter.author}
-              </h2>
-              <h2 className="m-2 ml-0">{`Last updated ${frontmatter.date}`}</h2>
-              {frontmatter.read_time != undefined ? (
-                <h3>{`${frontmatter.read_time} read`}</h3>
-              ) : (
-                <></>
-              )}
+            <div className="flex flex-row items-start p-5">
+              {frontmatter.tags.map((tag) => (
+                <Tag title={tag} key={tag} />
+              ))}
             </div>
           </div>
-          <div className="flex flex-row items-start p-5">
-            {frontmatter.tags.map((tag) => (
-              <Tag title={tag} key={tag} />
-            ))}
+          <div className="ml-2 mt-5">
+            <MDXProvider components={components}>
+              <MDXRemote {...mdxSource} />
+            </MDXProvider>
           </div>
+          <ShareBtns />
         </div>
-        <div className="ml-2 mt-5">
-          <MDXProvider components={components}>
-            <MDXRemote {...mdxSource} />
-          </MDXProvider>
-        </div>
-        <ShareBtns />
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
