23ee55431adbe2241a440f0c870cba721ef6cb1a
Brandon Peterson <brandonacfuntech@gmail.com>
2024-01-22 12:54:20 -0600

    Swap cors handling to middleware

--- next.config.js
+++ next.config.js
@@ -11,34 +11,6 @@ const withMDX = require("@next/mdx")({
     // If you use `MDXProvider`, uncomment the following line.
     // providerImportSource: "@mdx-js/react",
   },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Credentials",
-            value: String(
-              process.env.NEXT_PUBLIC_ACCESS_CONTROL_ALLOW_CREDENTIALS
-            ),
-          },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: String(process.env.NEXT_PUBLIC_ACCESS_CONTROL_ALLOW_ORIGIN),
-          }, // replace this your actual origin
-          {
-            key: "Access-Control-Allow-Methods",
-            value: String(process.env.NEXT_PUBLIC_ACCESS_CONTROL_ALLOW_METHODS),
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: String(process.env.NEXT_PUBLIC_ACCESS_CONTROL_ALLOW_HEADERS),
-          },
-        ],
-      },
-    ];
-  },
 });
 
 /** @type {import('next').NextConfig} */
