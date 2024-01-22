44e3a41cf416387923f1dbd70ff69a84563e8b49
Brandon Peterson <brandonacfuntech@gmail.com>
2024-01-22 13:03:12 -0600

    change CORS headers

--- pages/api/getPosts.ts
+++ pages/api/getPosts.ts
@@ -1,6 +1,12 @@
 import { getArticles, getNews, getQuestions } from "../../utils/getPosts";
 
 export default async function GET(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
   try {
     const articles = await getArticles(true);
     const questions = await getQuestions(true);
