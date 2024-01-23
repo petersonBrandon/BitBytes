7efa55e1232a22ba302d1b359af8d49e75ddd6aa
Brandon Peterson <brandonacfuntech@gmail.com>
2024-01-23 12:40:19 -0600

    Fix widths

--- pages/About.tsx
+++ pages/About.tsx
@@ -14,11 +14,11 @@ export default function About() {
         <link rel="icon" href="/favicon_new.ico" />
       </Head>
       <main className="w-full flex flex-col items-center mb-16 min-h-screen">
-        <div className="z-0 w-4/5 flex flex-col items-center pt-8 max-lg:w-11/12">
-          <div className="w-3/4 text-4xl pb-5 mb-6 flex flex-row justify-between max-lg:w-11/12">
+        <div className="z-0 w-full flex flex-col items-center pt-8 max-lg:w-11/12">
+          <div className="w-full text-4xl pb-5 mb-6 flex flex-row justify-between max-lg:w-11/12">
             <h2>About BitBytes</h2>
           </div>
-          <p className="w-3/4 text-xl max-lg:w-11/12">
+          <p className="w-full text-xl max-lg:w-11/12">
             {`Welcome to my blog! I\'m `}
             <Link
               href="https://www.brandonpeterson.dev"
@@ -32,10 +32,10 @@ export default function About() {
             learning journey. Join me as we explore the world of technology,
             learn together, and shape the future of software development.
           </p>
-          <div className="w-3/4 text-4xl pb-5 mb-6 mt-16 flex flex-row justify-between max-lg:w-11/12">
+          <div className="w-full text-4xl pb-5 mb-6 mt-16 flex flex-row justify-between max-lg:w-11/12">
             <h2>The Purpose of BitBytes</h2>
           </div>
-          <p className="w-3/4 text-xl max-lg:w-11/12">
+          <p className="w-full text-xl max-lg:w-11/12">
             {`BitBytes is a platform where I not only express my thoughts, conduct
             research, and share my ideas but also create a learning space for
             others. Through my writings, I aim to not only provide valuable
@@ -45,7 +45,7 @@ export default function About() {
             embark on this enlightening and enjoyable developer adventure
             together!`}
           </p>
-          <div className="flex flex-row items-center w-3/4 mt-16 max-lg:w-11/12">
+          <div className="flex flex-row items-center w-full mt-16 max-lg:w-11/12">
             <div className="w-36 h-36 rounded-full overflow-hidden m-3 max-lg:w-24 max-lg:h-24">
               <Image
                 src={"/Brandon.webp"}
