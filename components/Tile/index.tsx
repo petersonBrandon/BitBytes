1ad8983cc2102297eed303b28b44f2cdb05357ee
Brandon Peterson <brandonacfuntech@gmail.com>
2024-01-23 12:30:39 -0600

    Update tile UI

--- components/Tile/index.tsx
+++ components/Tile/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import Tag from "../Tag";
+import { HiOutlineArrowNarrowRight } from "react-icons/hi";
 
 interface TileTypes {
   slug: string;
@@ -35,11 +36,11 @@ const Tile: React.FC<TileTypes> = ({
         boxShadow: " 0px 0px 18px 4px rgba(237, 231, 227, 0.5)",
       }}
       key={slug}
-      className="w-2/4 border-2 bg-[#033977] bg-opacity-50 backdrop-blur-md border-white m-4 rounded-lg max-lg:w-full overflow-hidden group"
+      className="w-full border-2 bg-[#033977] bg-opacity-50 backdrop-blur-md border-white m-4 rounded-lg max-lg:w-full overflow-hidden group"
     >
       <Link href={`/${parent}/${slug}`} className="flex max-lg:flex-col">
         {image != null ? (
-          <div className="max-h-96 w-5/12 overflow-hidden flex flex-col items-center justify-center max-lg:w-full aspect-video">
+          <div className="max-h-52 w-1/2 overflow-hidden flex flex-col items-center justify-center max-lg:w-full aspect-video">
             <img
               src={image}
               alt="showcase_image"
@@ -49,33 +50,35 @@ const Tile: React.FC<TileTypes> = ({
         ) : (
           <></>
         )}
-        <section className="flex flex-col justify-between items-start p-5 pb-0 w-full space-y-3">
-          <div className="flex justify-between w-full max-lg:flex-col max-lg:space-y-3">
-            <div>
-              <h1 className="text-2xl mr-4">{title}</h1>
-              <h2 className="text-md mr-4">{subtitle}</h2>
+        <section className="flex justify-between items-start p-5 w-full">
+          <div className="space-y-3 flex flex-col justify-between items-start h-full">
+            <div className="flex justify-between w-full max-lg:flex-col max-lg:space-y-3">
+              <div className="space-y-2">
+                <h1 className="text-2xl">{title}</h1>
+                <p className="text-sm opacity-70">{`${read_time} - ${date}`}</p>
+                <h2 className="text-md">{subtitle}</h2>
+              </div>
             </div>
-            <div>
-              <p className="mt-2">{`${read_time}`}</p>
-              <div>{date}</div>
+            <div className="flex justify-between w-full">
+              <div className="flex flex-row items-start flex-wrap w-full max-lg:pt-5">
+                {tags.map((tag) => (
+                  <Tag title={tag} key={tag} />
+                ))}
+              </div>
+              {image_credits_link != null ? (
+                <div
+                  className="w-2/5 pl-5 pr-5 text-right text-xs pt-1"
+                  onClick={() => window.open(image_credits_link)}
+                >
+                  {image_credits_text}
+                </div>
+              ) : (
+                <></>
+              )}
             </div>
           </div>
-          <div className="flex justify-between w-full">
-            <div className="flex flex-row items-start pb-5 flex-wrap w-full max-lg:pt-5">
-              {tags.map((tag) => (
-                <Tag title={tag} key={tag} />
-              ))}
-            </div>
-            {image_credits_link != null ? (
-              <div
-                className="w-2/5 pl-5 pr-5 text-right text-xs pt-1"
-                onClick={() => window.open(image_credits_link)}
-              >
-                {image_credits_text}
-              </div>
-            ) : (
-              <></>
-            )}
+          <div className="opacity-0 mr-5 -translate-x-12 group-hover:-translate-x-0 group-hover:opacity-100 ease-in-out duration-300 h-full flex justify-center items-center max-lg:hidden">
+            <HiOutlineArrowNarrowRight className="w-10 h-10" />
           </div>
         </section>
       </Link>
